30b8a2b26961c556727523e4a65c1f3c6605e88a
HAL13000 <haru.smile.13@gmail.com>
2023-07-03 17:02:40 +0200

    commit by Haru

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import "./App.css";
-import "./pages/index.js";
-import "./pages/Work.js";
-import "./pages/About.js";
-import "./pages/Contact.js";
-import { BrowserRouter, Routes } from "react-router-dom";
+import Index from "./pages/Index/Index.js";
+import Work from "./pages/Work/Work.js";
+import About from "./pages/About/About.js";
+import Contact from "./pages/Contact/Contact.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* with "/" -> recognized as home */}
-        {/* routes */}
+        <Routes>
+          <Route path={"/"} element={<Index />} />
+          <Route path={"/work"} element={<Work />} />
+          <Route path={"/about"} element={<About />} />
+          <Route path={"/contact"} element={<Contact />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
+
+// Router ---> npm i react-router-dom
