00247ab742053cc9732c2235076feb6460ee1835
HAL13000 <haru.smile.13@gmail.com>
2023-07-10 09:37:58 +0200

    commit by Haru

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Work from "./pages/Work/Work.js";
 import About from "./pages/About/About.js";
 import Contact from "./pages/Contact/Contact.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeColor/ThemeContext";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path={"/"} element={<Index />} />
-          <Route path={"/work"} element={<Work />} />
-          <Route path={"/about"} element={<About />} />
-          <Route path={"/contact"} element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<Index />} />
+            <Route path={"/work"} element={<Work />} />
+            <Route path={"/about"} element={<About />} />
+            <Route path={"/contact"} element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 export default App;
